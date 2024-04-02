5902d50949e5bad2c3794f7cc5a47e4330f7f66f
wargesz <marcell.veszelyes@gmail.com>
2024-04-02 19:02:02 +0200

    encrypt/decrypt

--- decrypt.cpp
+++ decrypt.cpp
@@ -3,14 +3,14 @@
 int main() {
       std::string line,out = "";
       int salt = 17;
-      std::ifstream source("lipsum.txt");
+      std::ifstream source("encrypted.txt");
       while (getline(source,line)) {
             for (char c : line) {
-                  out+=c+salt*2;
+                  out+=c-salt*2;
             }
       }
       source.close();
-      std::ofstream output("encrypted.txt");
+      std::ofstream output("decrypted.txt");
       output<<out;
       output.close();
       return 0;
